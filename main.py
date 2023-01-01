c7cc375350e2f68b6b702e1dfd6ea01992450244
shohag-rana <mdshohag7613@gmail.com>
2023-01-01 22:20:15 +0600

    print function added

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ print(
     "The above URL, https://User_name@bitbucket.org/User_name/sample.git, \
         refers to your Bitbucket project URL"
 )
+print(
+    "The above URL, https://User_name@bitbucket.org/User_name/sample.git, \
+        refers to your Bitbucket project URL"
+)
+print(
+    "The above URL, https://User_name@bitbucket.org/User_name/sample.git, \
+        refers to your Bitbucket project URL"
+)
+print(
+    "The above URL, https://User_name@bitbucket.org/User_name/sample.git, \
+        refers to your Bitbucket project URL"
+)
+print(
+    "The above URL, https://User_name@bitbucket.org/User_name/sample.git, \
+        refers to your Bitbucket project URL"
+)
